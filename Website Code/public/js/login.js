5b8d4f494c8c51d48fff0e1d4092fda5e282fb77
ppu-ts <ppu-ts@users.noreply.github.com>
2022-02-20 15:58:50 -0500

    Add account creation, admin manage view
    - Add account sign up on frontend and backend
    - Add admin manage course view
    - Add logout code
    - Misc fixes/changes

--- Website Code/public/js/login.js
+++ Website Code/public/js/login.js
@@ -1,21 +1,5 @@
 (() => {
 
-  // to be removed when backend is implemented
-  const testUsers = {
-    "teacher1@test.edu": {
-      password: "test123",
-      role: "teacher"
-    },
-    "student1@test.edu": {
-      password: "test123",
-      role: "student"
-    },
-    "admin1@test.edu": {
-      password: "test123",
-      role: "admin"
-    }
-  };
-
   function showError(text) {
     document.getElementById("loginError").style.display = "block";
     document.getElementById("loginError").innerHTML = text;
@@ -31,16 +15,14 @@
       return showError("Please enter a valid email and password.")
     }
 
-
-    // fake API call to get a successful login message
-    let api = await apiCall("users/login", "POST", { email: email, password: password });
+    let api = await apiCall("users/login", "POST", {email: email, password: password});
     if (api.success) {
-		window.localStorage.setItem("user", JSON.stringify(api));
+      window.localStorage.setItem("user", JSON.stringify(api));
       if (api.role === "admin")
-        return document.location = "admin-department1.html";
+        return document.location = "admin-manage-sections.html";
       return document.location = `${api.role}-dashboard.html`;
     }
-     return showError("Incorrect email and/or password.")
+    return showError("Incorrect email and/or password.")
   }
 
   document.getElementById("loginForm").addEventListener("submit", login);
--- Website Code/routes/users.js
+++ Website Code/routes/users.js
@@ -32,4 +32,61 @@ router.post('/login', validate({body: LoginSchema}), function (req, res, next) {
   });
 });
 
+const SignUpSchema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'string',
+      required: true,
+      format: 'email',
+    },
+    password: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+/*
+ /signup
+ step 1: check for existing user under that email address
+ step 2: insert user into users table
+ step 3: select user to get their info and key to log them in on the frontend
+ */
+router.post('/signup', validate({body: SignUpSchema}), function (req, res, next) {
+  // step 1
+  query("select `id` from `USERS` where `Email Address` = ?", [
+    req.body.email,
+  ], data => {
+    if (data.length !== 0) {
+      // account with email already exists
+      return res.send({success: false, error: "Account already exists"});
+    }
+    let name = req.body.name.split(" ");
+    // step 2
+    query("insert into `USERS` values (null, ?, ?, null, ?, 'student', sha1(?), sha1(concat(now(), `ID`, `NAME`)))",
+        [
+          name[0],
+          name[1],
+          req.body.email,
+          req.body.password
+        ],
+        (data) => {
+          // step 3
+          query("select `ID` as `id`, `NAME` as `name`, `User Role` as `role`, sha1(concat(now(), `ID`, `NAME`)) as `key` from `USERS` where `ID` = ?", [
+            data.insertId
+          ], data => {
+            if (!data || !data.length) {
+              return res.send({success: false});
+            }
+            return res.send({success: true, ...data[0]});
+          });
+        });
+  });
+});
+
 module.exports = router;
