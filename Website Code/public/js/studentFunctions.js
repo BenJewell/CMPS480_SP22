90c4c4542b221120402d164a7e2b89b7ffa8a7c8
BenJewell <97567577+BenJewell@users.noreply.github.com>
2022-02-28 16:46:30 -0500

    Added color coding on student dashboard page

--- Website Code/public/js/studentFunctions.js
+++ Website Code/public/js/studentFunctions.js
@@ -1,33 +1,56 @@
-function letterGrade(num){
-    if(num >= 0.9){
+function letterGrade(num) {
+    if (num >= 0.9) {
         return "A";
     }
-    else if(num >= 0.8){
+    else if (num >= 0.8) {
         return "B";
     }
-    else if(num >= 0.7){
+    else if (num >= 0.7) {
         return "C";
     }
-    else if(num >= 0.6){
+    else if (num >= 0.6) {
         return "D";
     }
-    else if(num < 0.6){
+    else if (num < 0.6) {
         return "F";
     }
     return "";
 }
-function toPercentage(num){
+
+// This sets row colors by grade. Setting row colors if missing is being done in a different way.
+function rowColor(num) {
+    if (num >= 0.9) {
+        return "hsla(160, 100%, 50%, 0.3)";
+    }
+    else if (num >= 0.8) {
+        return "hsla(100, 100%, 50%, 0.3)";
+    }
+    else if (num >= 0.7) {
+        return "hsla(60, 100%, 50%, 0.3)";
+    }
+    else if (num >= 0.6) {
+        return "hsla(30, 100%, 50%, 0.3)";
+    }
+    else if (num < 0.6) {
+        return "hsla(0, 100%, 50%, 0.3)";
+    }
+    return "";
+}
+
+function toPercentage(num) {
     return Math.round(num * 10000) / 100;
 }
-function checkNull(val){
-    if(val == null){
+
+function checkNull(val) {
+    if (val == null) {
         return "-";
     }
     return val;
 }
-function checkMissing(miss){
+
+function checkMissing(miss) {
     var warning = feather.icons['alert-circle'].toSvg();
-    if(miss){
+    if (miss) {
         document.getElementById("missingTR").style = "background-color: hsla(0, 100%, 50%, 0.3)";
         document.getElementById("missingTD").innerHTML = warning;
 
