9578eb66a9a6faa33a58b96854840f313fe3190c
Foglia <emfogli@pointpark.edu>
2022-03-24 00:24:32 -0400

    adding printable assignment calendar and updating nav bar

--- Website Code/routes/student.js
+++ Website Code/routes/student.js
@@ -18,7 +18,7 @@ router.get('/courses', auth.verifySessionAndRole("student"), function (req, res,
 
 //Course Grades Table
 router.get('/grades/:id', auth.verifySessionAndRole("student"), function (req, res, next) {
-  query(`SELECT Assignments.name, Grades.points_received, Assignments.points_possible, DATE_FORMAT(Assignments.due_date, '%m/%d/%y %h:%i %p') AS due_date, Grades.missing, Grades.instructor_notes, Grades.flagged_for_audit FROM Assignments, Grades WHERE Assignments.assignment_id = Grades.assignment_id AND Assignments.section_id = ? AND Grades.student_id = ? ORDER BY due_date DESC, name;`, [req.params.id, res.locals.userId], table => {
+  query(`SELECT Assignments.assignment_category, Assignments.name, Grades.points_received, Assignments.points_possible, DATE_FORMAT(Assignments.due_date, '%m/%d/%y %h:%i %p') AS due_date, Grades.missing, Grades.instructor_notes, Grades.flagged_for_audit FROM Assignments, Grades WHERE Assignments.assignment_id = Grades.assignment_id AND Assignments.section_id = ? AND Grades.student_id = ? ORDER BY assignment_category, due_date DESC, name;`, [req.params.id, res.locals.userId], table => {
     query("SELECT (SUM(Grades.points_received)/SUM(Assignments.points_possible)) AS total_grade FROM Assignments, Grades WHERE Assignments.assignment_id = Grades.assignment_id AND Assignments.section_id = ? AND Grades.student_id = ? AND Grades.points_received IS NOT NULL", [req.params.id, res.locals.userId], totalGrade => {
       return res.send({...totalGrade[0], table: table});
     });
@@ -67,5 +67,19 @@ router.get('/dashboard/upcoming', auth.verifySessionAndRole("student"), function
     return res.send(d);
   });
 });
-
+//Assignment Calendar
+router.get('/calendar', auth.verifySessionAndRole("student"), function (req, res, next) {
+  query(`SELECT Courses.course_id, Courses.name AS course_name, Assignments.name AS assignment_name, DATE_FORMAT(Assignments.due_date, '%m/%d/%y') as due_date, DATE_FORMAT(Assignments.due_date, '%h:%i %p') as due_time 
+          FROM Assignments, Sections, Courses, Section_Registrations
+          WHERE student_id = ?
+          AND Sections.section_id = Assignments.section_id = Section_Registrations.section_id
+          AND Courses.course_id = Sections.course_id 
+          AND Assignments.due_date > now() 
+          ORDER BY Assignments.due_date
+  `, [res.locals.userId], table => {
+    query("SELECT Courses.course_id, name AS course_name FROM Sections, Courses, Section_Registrations WHERE student_id = ? AND Sections.section_id = Section_Registrations.section_id AND Courses.course_id = Sections.course_id ", [res.locals.userId], courses => {
+      return res.send({courses: courses, table: table});
+    });
+  });
+});
 module.exports = router;
