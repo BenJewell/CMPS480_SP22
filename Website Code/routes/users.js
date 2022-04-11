8c1abdbd2b1115f0e1615249ed1c6d9c5f3a83e2
ppu-ts <ppu-ts@users.noreply.github.com>
2022-04-11 12:20:07 -0400

    Add admin and user settings

--- Website Code/routes/users.js
+++ Website Code/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {query, log_action} = require("../util/db");
 const router = express.Router();
 const validate = require('express-jsonschema').validate;
+const auth = require("../middleware/auth");
 
 const LoginSchema = {
   type: 'object',
@@ -31,6 +32,53 @@ router.post('/login', validate({body: LoginSchema}), function (req, res, next) {
     });
     log_action(res.locals.userId, `logged into the system succesfully ${req.body.student_id}`, req.params.id, "Users")
     return res.send({success: true, ...data[0]});
+  });
+});
+
+const SettingsSchema = {
+  type: 'object',
+  properties: {
+    email_address: {
+      type: 'string',
+      required: true,
+      format: 'email',
+    },
+    phone_number: {
+      type: 'string',
+      required: true,
+    },
+    old_password: {
+      type: 'string',
+      required: false
+    },
+    new_password: {
+      type: 'string',
+      required: false
+    }
+  }
+};
+
+router.get('/settings', auth.verifySession(), function (req, res, next) {
+  query("select email_address, phone_number from Users where user_id = ?", [res.locals.userId], d => {
+    return res.send(d[0]);
+  });
+});
+
+router.put('/settings', validate({body: SettingsSchema}), auth.verifySession(), function (req, res, next) {
+
+  query("update Users set email_address = ?, phone_number = ? where user_id = ?", [req.body.email_address, req.body.phone_number, res.locals.userId], _ => {
+    if (req.body.new_password !== "") {
+      // update password by checking user_id + old password in query
+      query("update Users set password = sha1(?) where user_id = ? and password = sha1(?)", [
+        req.body.new_password,
+        res.locals.userId,
+        req.body.old_password,
+      ], _ => {
+        return res.send({success: true})
+      })
+    } else {
+      return res.send({success: true})
+    }
   });
 });
 
