2de382c1ba51abdeccac951c2ed67a5dc6f5aacf
BenJewell <97567577+BenJewell@users.noreply.github.com>
2022-04-10 23:30:48 -0400

    Added error if login times out

--- Website Code/public/js/login.js
+++ Website Code/public/js/login.js
@@ -5,6 +5,12 @@
     document.getElementById("loginError").innerHTML = text;
   }
 
+  // Let the user know if their session timed out
+  const url = new URL(window.location.href);
+  if (url.searchParams.get("timeout")) {
+    return showError("You have been signed out due to 15 minutes of inactivity.")
+  };
+
   async function login(event) {
     event.preventDefault();
 
@@ -15,14 +21,14 @@
       return showError("Please enter a valid email and password.")
     }
 
-    let api = await apiCall("users/login", "POST", {email: email, password: password});
+    let api = await apiCall("users/login", "POST", { email: email, password: password });
     if (api.success) {
       window.localStorage.setItem("user", JSON.stringify(api));
       if (api.role === "admin")
         return document.location = "admin-manage-sections.html";
       return document.location = `${api.role}-dashboard.html`;
     }
-    return showError("Incorrect email and/or password.")
+    return showError("Incorrect email and/or password. If you forgot your password, contact an administrator to have it reset.")
   }
 
   document.getElementById("loginForm").addEventListener("submit", login);
